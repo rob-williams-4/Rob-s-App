fe8184750a60ac001bd1a1993f38d39884334c1d
rob-williams-4 <rob.williams.af@gmail.com>
2023-04-06 19:00:17 -0400

    Initial Commit

---
+++ Rob's App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rob's App
+//
+//  Created by Rob Williams on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rob's App/Rob_s_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rob_s_AppApp.swift
+//  Rob's App
+//
+//  Created by Rob Williams on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Rob_s_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
